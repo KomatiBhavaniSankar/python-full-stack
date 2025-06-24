cab6d30a3fb7f70aaf6589d4dd109175f20925f0
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-06-24 10:09:07 +0530

    Initial commit

---
+++ ah.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+
+//let , const, var used to declare in js
+
+let a =10;
+//let is block scop, cant be redeclared in the same scope
+
+const b =20;
+
+//consr is blocked and not reassigned
+
+var c =30;
+console.log(a, b,c);
+
+
+
+// datatypes in js
+// data types 
