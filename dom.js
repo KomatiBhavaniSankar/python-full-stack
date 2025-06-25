bdfb81e622f265f3e988914547955c1efc0e1de4
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-06-25 15:11:49 +0530

    Initial commit

---
+++ dom.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = "lightblue";
+
+document.getElementById("header").innerHTML = "Hello world";
+
+document.getElementById("header").style.color="red";
+
+document.getElementById("para").style.color="blue";
+document.getElementById("para1").style.color="red";
+document.getElementById("para2").style.color="pink";
+
+document.getElementsByClassName("ds")[0].style.color="green";
+document.getElementsByClassName("ds")[1].style.color="blue";
+document.getElementsByClassName("ds")[2].style.color="red";
