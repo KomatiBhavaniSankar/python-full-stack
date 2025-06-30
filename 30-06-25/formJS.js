65e60b37cb676706e4c3db10d148c30dfabb9f59
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-06-30 12:40:56 +0530

    FORM MORE SIMPLE

--- 30-06-25/formJS.js
+++ 30-06-25/formJS.js
@@ -1,53 +1,27 @@
+const form = document.getElementById("studentForm");
 
-var form = document.getElementById("studentForm");
-
-
-var fields = [
-  { label: "First Name", type: "text", name: "firstName" },
-  { label: "Last Name", type: "text", name: "lastName" },
-  { label: "Phone Number", type: "tel", name: "phone" },
-  { label: "Roll Number", type: "text", name: "rollNo" },
-  { label: "Photo", type: "file", name: "photo" },
-  { label: "Email", type: "email", name: "email" },
-  { label: "Address", type: "textarea", name: "address" },
-  { label: "Parent's Name", type: "text", name: "parentName" },
-  { label: "Parent's Phone", type: "tel", name: "parentPhone" },
-  { label: "Class", type: "text", name: "class" },
-  { label: "Section", type: "text", name: "section" }
-];
-
-var heading = document.createElement("h2");
-heading.innerText = "Student Data Form";
-form.appendChild(heading);
-
-for (var i = 0; i < fields.length; i++) {
-  var field = fields[i];
-
-  var label = document.createElement("label");
-  label.htmlFor = field.name;
-  label.innerText = field.label;
-  form.appendChild(label);
-
- 
-  var input;
-  if (field.type === "textarea") {
-    input = document.createElement("textarea");
-  } else {
-    input = document.createElement("input");
-    input.type = field.type;
-  }
-
-  input.id = field.name;
-  input.name = field.name;
-  form.appendChild(input);
-
-  
-  form.appendChild(document.createElement("br"));
-}
+  const fields = [
+    "First Name", "Last Name", "Phone Number", "Roll Number",
+    "Photo", "Email", "Address", "Parent's Name",
+    "Parent's", "Class", "Section"
+  ];
 
 
-var button = document.createElement("button");
-button.type = "submit";
-button.innerText = "Submit";
-form.appendChild(button);
+  const heading = document.createElement("h2");
+  heading.textContent = "Student Data Form";
+  form.appendChild(heading);
 
+fields.forEach(field => {
+  form.innerHTML += `
+    <label>${field}</label><br>
+    ${field === "Address" 
+      ? '<textarea></textarea>' 
+      : '<input type="text">'}
+    <br>
+  `;
+});
+
+  const button = document.createElement("button");
+  button.type = "submit";
+  button.textContent = "Submit";
+  form.appendChild(button);
