3308aeee2ee5d88b2427be87b62f1b40aa6d9552
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-07-01 12:32:44 +0530

    callback function

---
+++ index.js
@@ -0,0 +1,26 @@
+//console.log("hello world");
+
+//function sayHello(){
+
+  //  console.log("hello function");
+//}
+
+//sayHello();
+//console.log("new");
+
+console.log("one");
+console.log("2");
+
+setTimeout(()=> {
+
+console.log("hello world");
+
+},5000);
+console.log("three");
+console.log("four");
+
+function sum(a,b){
+
+    console.log(a+b);
+}
+sum(1,3);
