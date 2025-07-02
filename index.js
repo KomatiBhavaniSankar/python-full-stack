c79a11d85e3fdfc601fa2cb040c7f1e55b4862a4
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-07-02 12:37:05 +0530

    02-07-25

--- index.js
+++ index.js
@@ -146,3 +146,19 @@ async function fetchData(){
         console.error("error occured", error);
     }
 }
+
+fetchData();
+async function getnextdata(){
+console.log("getting data 1");
+await getData(1)
+
+console.log("getting data 2");
+await getData(2)
+
+
+console.log("getting data 3");
+await getData(3)
+
+}
+
+getnextdata();
